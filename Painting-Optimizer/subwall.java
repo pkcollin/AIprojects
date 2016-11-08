39d6f4fa05da9610cce8a4645b961382d34c094a
Patrick Collins <doodlmyr@gmail.com>
2016-11-07 22:44:59 -0500

    Create subwall.java

---
+++ Painting-Optimizer/subwall.java
@@ -0,0 +1,17 @@
+/**
+ * Created by liberato on 9/16/14.
+ */
+public class SubWall {
+    public final int x;
+    public final int y;
+    public final int width;
+    public final int height;
+
+    public SubWall(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+}
+
